afdfdc7cc46ff61cfa91656d3a5621faf5ba6b50
Just Sala Farre <justmaister@gmail.com>
2025-03-05 22:44:33 +0100

    Create activities data

--- api_app/__main__.py
+++ api_app/__main__.py
@@ -5,13 +5,14 @@ from .utils.api_client import APIClient
 def main():
     api_cache = APICache()
     data = api_cache.get_access_token()
-    print("Data from API:", data)
 
-    access_token = os.getenv('ACCESS_TOKEN')
+    # access_token = data['access_token']
+    access_token = os.getenv('ACCESS_TOKEN') # For developing purpouses
     client = APIClient(access_token)
 
     client.fetch_and_save_athlete_data()
-    client.fetch_and_save_activities_data(page=1, per_page=10)
+    client.fetch_and_save_athlete_activities_data(page=1, per_page=10)
+    client.fetch_and_save_activities_data()
 
 if __name__ == "__main__":
     main()
--- api_app/utils/api_client.py
+++ api_app/utils/api_client.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class APIClient:
+    ALLOWED_GROUPS = ['activities', 'routes']
     def __init__(self, access_token: str):
         self.access_token = access_token
         self.headers = {
@@ -29,19 +30,25 @@ class APIClient:
             logging.error("An error occurred: %s", err)
             return None
 
-    def save_json_to_file(self, data: dict, filename: str):
-        data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-        os.makedirs(data_dir, exist_ok=True)
+    def save_json_to_file(self, data: dict, filename: str, group: str = ''):
+        if group and group not in self.ALLOWED_GROUPS:
+            raise ValueError(f"Invalid group: {group}. Allowed groups are: {', '.join(self.ALLOWED_GROUPS)}")
 
+        data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+        if group:
+            data_dir = os.path.join(data_dir, group)
+        os.makedirs(data_dir, exist_ok=True)
         file_path = os.path.join(data_dir, filename)
 
         with open(file_path, 'w') as json_file:
             json.dump(data, json_file, indent=4)
         logging.info("Data saved to %s", file_path)
 
-    def get_athlete_data(self) -> Dict:
+    def fetch_and_save_athlete_data(self):
         athlete_url = 'https://www.strava.com/api/v3/athlete'
-        return self.make_request(athlete_url)
+        athlete_data = self.make_request(athlete_url)
+        if athlete_data:
+            self.save_json_to_file(athlete_data, 'athlete_data.json')
 
     def get_athlete_activities_data(self, page: int = 1, per_page: int = 200) -> Dict:
         athlete_activities_url = 'https://www.strava.com/api/v3/athlete/activities'
@@ -49,16 +56,16 @@ class APIClient:
         self.headers['per_page'] = str(per_page)
         return self.make_request(athlete_activities_url)
 
-    def fetch_and_save_athlete_data(self):
-        athlete_data = self.get_athlete_data()
-        if athlete_data:
-            self.save_json_to_file(athlete_data, 'athlete_data.json')
+    def fetch_and_save_athlete_activities_data(self, page: int = 1, per_page: int = 200):
+        athlete_activities_data = self.get_athlete_activities_data(page=page, per_page=per_page)
+        if athlete_activities_data:
+            self.save_json_to_file(athlete_activities_data, 'athlete_activities_data.json')
 
-    def fetch_and_save_activities_data(self, page: int = 1, per_page: int = 200):
-        activities_data = self.get_athlete_activities_data(page=page, per_page=per_page)
-        if activities_data:
-            self.save_json_to_file(activities_data, 'activities_data.json')
-
-    def extract_activity_ids(self, activities_data) -> List:
-        return [activity['id'] for activity in activities_data]
-
+    def fetch_and_save_activities_data(self):
+        athlete_activities_data = self.get_athlete_activities_data()
+        activities_ids_list = [activity['id'] for activity in athlete_activities_data]
+        print(activities_ids_list)
+        for activity_id in activities_ids_list:
+            activity_url = f'https://www.strava.com/api/v3/activities/{activity_id}?include_all_efforts=true'
+            activity_id_data = self.make_request(activity_url)
+            self.save_json_to_file(activity_id_data, f'activity_{activity_id}.json', 'activities')
